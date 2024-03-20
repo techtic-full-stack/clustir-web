52e6c9bcf84cc5b9f6d3717399670a9c67c384f0
yashtechtic <techtic.yashgajjar@gmail.com>
2024-03-20 17:17:27 +0530

    design changes

--- clustir-frontend/src/components/auth/AuthLayout.tsx
+++ clustir-frontend/src/components/auth/AuthLayout.tsx
@@ -9,16 +9,16 @@ const Layout = ({ children }: LayoutWrapperType) => {
   return (
     <div className="min-h-screen flex flex-col justify-between bg-[#F8F8F8]">
       <div className="flex flex-col justify-center items-center relative">
-        <div className="mt-[100px] w-[600px]">
+        <div className="mt-[30px] w-[600px] ">
           <div className="flex items-center justify-center">
             <ClustirLogo />
           </div>
-          <div className="mt-[45px] bg-[#FFFFFF]">{children}</div>
+          <div className="mt-[30px] bg-[#FFFFFF] rounded-md">{children}</div>
         </div>
       </div>
 
       {/* Footer */}
-      <footer className="bg-[#F4F4F4] py-4  text-sm h-[77px] flex items-center">
+      <footer className="bg-[#F4F4F4] py-4  text-sm h-[77px] flex items-center mt-[30px]">
         <div className="flex pl-[77px]">
           <div className="font-brother font-normal text-base leading-tight">
             ©2023 Clustir, Inc.
--- clustir-frontend/src/components/createAccount/index.tsx
+++ clustir-frontend/src/components/createAccount/index.tsx
@@ -1,4 +1,9 @@
+import { Input } from "antd";
 import React from "react";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Typography } from "antd";
+
+const { Paragraph } = Typography;
 
 function CreateAccount() {
   return (
@@ -7,9 +12,54 @@ function CreateAccount() {
         <div className="flex justify-center items-center font-[700] text-[25px]  text-[#000000]">
           Create an account
         </div>
+
         <div className="mt-[30px] ">
-          <div className="font-[700] text-[18px] font-brother text-[#000000]">
-            Email
+          <div className="mb-[20px]">
+            <div className="font-[700] text-[18px] font-brother text-[#000000] mb-[7px]">
+              Email
+            </div>
+
+            <Input
+              placeholder="Basic usage"
+              className="h-12 px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div>
+            <div className="font-[700] text-[18px] font-brother text-[#000000] mb-[7px]">
+              Password
+            </div>
+            <Input.Password
+              className="h-12 px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500 mb-[50px]"
+              placeholder="input password"
+              iconRender={(visible) =>
+                visible ? (
+                  <EyeTwoTone style={{ fontSize: "20px" }} />
+                ) : (
+                  <EyeInvisibleOutlined style={{ fontSize: "20px" }} />
+                )
+              }
+            />
+          </div>
+          <button className="w-full h-12 bg-[#4C45EE]  font-[700] letter-spacing-normal text-[15px] text-white py-2 px-4 rounded-md">
+            Primary Button
+          </button>
+          <div className="mt-[30px] w-[400px] ">
+            <span className="text-[14px] font-[400] flex justify-center items-center">
+              By creating an account you agree to the
+            </span>
+            <Paragraph className=" flex justify-center items-center mb-[20px]">
+              <span className="text-[#4C45EE] text-[14px] mr-[6px] font-[500]">
+                Terms of Service
+              </span>{" "}
+              and{" "}
+              <span className="text-[#4C45EE] text-[14px] ml-[6px] font-[500]">
+                Privacy Policy Copy
+              </span>
+            </Paragraph>
+
+            <div className="text-[14px] flex justify-center items-center">
+              Have an account? Login
+            </div>
           </div>
         </div>
       </div>
