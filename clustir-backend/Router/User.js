1d4d5f53d9ea73d3507738eac23ef0d2acc21dee
rjsharma24 <rajesh@techtic.agency>
2024-03-21 14:24:02 +0530

    Remove unnecessary thing

--- clustir-backend/Router/User.js
+++ clustir-backend/Router/User.js
@@ -4,12 +4,10 @@ const router = express.Router();
 const { register, verifyUser, resendOtp, loginUser, addMurchantBusiness } = require("../Controller");
 const { authenticateToken } = require("../Utils/commonFile");
 
-// router.post("/login", login);
 router.post("/register", register);
 router.patch("/verifyUser", verifyUser);
 router.patch("/resendOtp", resendOtp);
 router.post("/login", loginUser);
 router.post("/addBusiness", authenticateToken, addMurchantBusiness);
-// router.post("/verify", verify); // Assuming you have a separate function for verification
 
 module.exports = router;
