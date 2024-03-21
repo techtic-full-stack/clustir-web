2c3956ed627cf1d87ea3d1918536e0beda8f7dfb
dolly-techtic <dolly@techtic.agency>
2024-03-21 11:44:01 +0000

    env update

--- clustir-backend/index.js
+++ clustir-backend/index.js
@@ -29,7 +29,7 @@ db.mongoose
 app.use("/api/user", require("./Router/User"));
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4001 ;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
